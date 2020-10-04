e48f5451e5eb96598c0f6b2b202af918d2bba0fc
Ross Eby <44152538+rmeby@users.noreply.github.com>
2020-10-04 14:11:46 -0400

    Change buttons to SVG

--- src/components/WindowControls.vue
+++ src/components/WindowControls.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <button @click="minimizeWindow">MIN</button>
-        <button @click="closeWindow">CLS</button>
+        <svg @click="minimizeWindow" class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4"/></svg>
+        <svg @click="closeWindow" class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/></svg>
     </div>
 </template>
 
