62c669b7c3be77741332ccc3cfa52654787aa54b
Ross Eby <44152538+rmeby@users.noreply.github.com>
2020-10-04 15:13:36 -0400

    Remove outlines, background hover

--- src/components/WindowControls.vue
+++ src/components/WindowControls.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex">
         <div>
-            <button>
-                <svg @click="minimizeWindow" class="w-5 h-5 hover:bg-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#a0aec0"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4"/></svg>
+            <button class="focus:outline-none">
+                <svg @click="minimizeWindow" class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#a0aec0"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4"/></svg>
             </button>
         </div>
         <div>
-            <button>
-                <svg @click="closeWindow" class="w-5 h-5 hover:bg-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#a0aec0"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/></svg>
+            <button class="focus:outline-none">
+                <svg @click="closeWindow" class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#a0aec0"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/></svg>
             </button>
         </div>
     </div>
@@ -29,6 +29,6 @@ export default {
 
 <style scoped>
 svg:hover {
-    stroke:black
+    stroke: #4a5568
 }
 </style>
