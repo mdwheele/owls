7839003c215418ca54cc43743cd520371eca9956
Dustin Wheeler <mdwheele@gmail.com>
2020-10-06 09:52:58 -0400

    Adjust styling of WindowControls

--- src/components/WindowControls.vue
+++ src/components/WindowControls.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="flex">
-    <div>
-      <button class="focus:outline-none">
-        <svg @click="minimizeWindow" class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#a0aec0"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4"/></svg>
-      </button>
-    </div>
-    <div>
-      <button class="focus:outline-none">
-        <svg @click="closeWindow" class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#a0aec0"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/></svg>
-      </button>
-    </div>
+  <div class="flex space-x-2">
+    <button class="focus:outline-none" @click="minimizeWindow">
+      <!-- https://heroicons.com (minus-sm) -->
+      <svg class="w-5 h-5 text-gray-500 hover:text-gray-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />
+      </svg>
+    </button>
+    <button class="focus:outline-none" @click="closeWindow">
+      <!-- https://heroicons.com (x) -->
+      <svg class="w-5 h-5 text-gray-500 hover:text-gray-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+      </svg>
+    </button>
   </div>
 </template>
 
