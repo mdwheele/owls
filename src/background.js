ab658cfa98c35fdce405ca378ee22b7760cb593e
Ross Eby <44152538+rmeby@users.noreply.github.com>
2020-10-13 13:57:05 -0400

    Add shadow to window
    Towards #9 .
    
    Still having an issue with the window height.

--- src/background.js
+++ src/background.js
@@ -20,8 +20,8 @@ protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({ 
-    width: 800, 
-    height: 600, 
+    width: 780, 
+    height: 660, 
     frame: false,
     resizable: false,
     transparent: true,
